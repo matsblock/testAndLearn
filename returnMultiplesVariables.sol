32e2f9989079451bd2fa18c1767b6cdfbb3b7d94
matsblock <96005274+matsblock@users.noreply.github.com>
2022-03-14 18:45:38 -0300

    Create returnMultiplesVariables.sol

---
+++ returnMultiplesVariables.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract sample 
+{ 
+
+
+    int public iTest;
+    string public sTest;
+
+    function test() public {
+        (uTest, sTest) = a(); 
+     }
+
+    function a() public returns (int a, string memory c) 
+    { 
+        return (1, "ss"); 
+    } 
+
+    function b() public 
+    { 
+        int A; 
+        string memory B; 
+      
+
+        //A is 1 and B is "ss" 
+        (A, B) = a(); 
+
+        //A is 1 
+        (A,) = a(); 
+
+        //B is "ss" 
+        (, B) = a();
+
+
+    } 
+} 
+
